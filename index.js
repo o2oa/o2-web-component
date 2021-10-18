1d03c3be0e5f585c73f166cf97c1a808411833f9
huqi <hq_1980@hotmail.com>
2021-10-18 20:03:34 +0800

    init

---
+++ index.js
@@ -0,0 +1,67 @@
+import { createApp } from 'vue';
+
+const o2 = window.o2;
+const layout = window.layout;
+
+class Component{
+    constructor(name, app, options){
+        this.name = name;
+        this.options = options;
+        this.vueApp = app;
+        this._initComponent();
+        this._defineComponent();
+    }
+    _initComponent() {
+        const op = (this.options) || {};
+        const path = this.name.split('.');
+        this.component = o2.xApplication;
+        path.forEach((p) => {
+            this.component = this.component[p] = this.component[p] || {};
+        });
+        if (!this.component.options) this.component.options = {};
+        this.component.options.multitask = !!op.multitask;
+    }
+    _defineComponent(){
+        const op = (this.options) || {};
+        const app = this.vueApp;
+        const component = this.component;
+        component.Main = new window.Class({
+            Extends: o2.xApplication.Common.Main,
+            Implements: [window.Options, window.Events],
+            options: Object.assign(this._defaultComponentOptions(), op),
+            onQueryLoad: function (){
+                this.lp = component.LP;
+            },
+            loadApplication: function(callback){
+                this.vueApp = app;
+                app.mounted = callback;
+                createApp(app).mount(this.content);
+            }
+        });
+    }
+    _defaultComponentOptions(){
+        return {
+            "style": "default",
+            "name": this.name,
+            "mvcStyle": "style.css",
+            "icon": "icon.png",
+            "title": this.component.LP.title
+        }
+    }
+    render(){
+        Promise.resolve(((layout.session && layout.session.user) || layout.sessionPromise)).then(()=>{
+            layout.apps = [];
+            layout.openApplication(null, this.name);
+        });
+    }
+}
+
+const loadComponent = function(name, app, options){
+    return new Promise((resolve)=>{
+        layout.addReady(()=>{
+            const c = new Component(name, app, (options || {}));
+            resolve(c);
+        });
+    });
+}
+export {o2, layout, loadComponent};
